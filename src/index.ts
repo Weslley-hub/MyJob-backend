996510a80dd03923fd90206a681f8c79171379de
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2024-09-06 20:46:37 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors';
 import { createUser } from './controllers/User/userController';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(json());
 
@@ -11,3 +14,4 @@ app.post('/register', createUser);
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
