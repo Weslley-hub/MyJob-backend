660f5c2766430522084435abf531ff8c07c0b953
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2024-09-15 17:11:28 -0300

    listar usuarios

---
+++ src/services/User/listService.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// export async function getUserById(id) {
+//   try {
+//     const user = await prisma.user.findUnique(id);
+//     return user;
+//   } catch (error) {
+//     throw new Error('Erro ao buscar Usuario');
+//   }
+// }
+
+export async function getUsersAll() {
+    try {
+      const user = await prisma.user.findMany();
+      return user;
+    } catch (error) {
+      throw new Error('Erro ao buscar Usuarios');
+    }
+  }
