394820e7233a13a3594871d156517c96ad09250b
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2024-09-15 17:33:41 -0300

    retornando objeto user, message e status

--- src/controllers/User/userController.ts
+++ src/controllers/User/userController.ts
@@ -4,21 +4,41 @@ import { registerUser } from '../../services/User/registerService';
 import { getUsersAll } from '../../services/User/listService';
 
 export async function loginUser(req: Request, res: Response) {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
-  
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Preencha todas as informações' });
+  if (!email || !password) {
+    return res.status(400).json({
+      status: 'error',
+      code: 400,
+      message: 'Preencha todas as informações'
+    });
+  }
+
+  try {
+    const { token, user } = await authenticateUser(email, password);
+
+    res.json({
+      status: 'success',
+      code: 200,
+      token,
+      user
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(401).json({
+        status: 'error',
+        code: 401,
+        message: error.message
+      });
+    } else {
+      res.status(500).json({
+        status: 'error',
+        code: 500,
+        message: 'Erro interno do servidor'
+      });
     }
-  
-    try {
-      const { token } = await authenticateUser(email, password);
-      res.json({ token });
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(401).json({ error: error.message });
-    }
-}}
+  }
+}
 
 export async function createUser(req: Request, res: Response) {
   const { name, email, password, categoriaId } = req.body;
--- src/services/User/loginService.ts
+++ src/services/User/loginService.ts
@@ -6,20 +6,33 @@ const prisma = new PrismaClient();
 const jwtSecret = process.env.JWT_SECRET || ''
 
 export async function authenticateUser(email: string, password: string) {
-    const user = await prisma.user.findUnique({
-      where: { email },
-    });
-  
-    if (!user) {
-      throw new Error('Usuario não existe');
-    }
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    
-    if (!passwordMatch) {
-      throw new Error('Senha invalida');
-    }
-  
-    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '1h' });
-  
-    return { token };
+  const user = await prisma.user.findUnique({
+    where: { email },
+    select: { 
+      id: true,
+      name: true,
+      email: true,
+      password: true
+    },
+  });
+
+  if (!user) {
+    throw new Error('Usuário não existe');
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    throw new Error('Senha inválida');
+  }
+
+  const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '1h' });
+
+  return {
+    token,
+    user: {
+      name: user.name,
+      email: user.email,
+    },
+  };
 }
