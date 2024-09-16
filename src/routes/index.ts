6d4db08cfc2d33a0a58e150efa278e887f17b0d0
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2024-09-15 22:44:27 -0300

    routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,19 @@ import express from 'express';
 import categoryRoutes from './categoryRoutes';
 import userRoutes from './userRoutes';
 import authRoutes from './authRoutes';
+import experienceRoutes from './experienceRoutes';
+import educationRoutes from './educationRoutes';
 
 const router = express.Router();
 
 router.use('/auths', authRoutes);
-router.use('/categories', categoryRoutes);
+
 router.use('/users', userRoutes);
+
+router.use('/professional-experiences', experienceRoutes);
+
+router.use('/categories', categoryRoutes);
+
+router.use('/educations', educationRoutes);
 
 export default router;
